8d6349579e80ce956096cffe30eb183259d98546
Vincent Chang <changvincent57@gmail.com>
2021-05-16 05:53:21 +0800

    rn

--- RNNg.py
+++ RNNg.py
@@ -2,6 +2,21 @@ import matplotlib.pyplot as plt
 from generateData import generateSet
 import tensorflow as tf
 from tensorflow.keras.layers.experimental import preprocessing
+import os
+import datetime
+
+import IPython
+import IPython.display
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+
+mpl.rcParams['figure.figsize'] = (8, 6)
+mpl.rcParams['axes.grid'] = False
+
 #meta
 trainSize = 10
 ##
@@ -10,6 +25,7 @@ trainXY = [generateSet(i, 10, (1,10)) for i in range(1,trainSize)]
 
 def buildrnn():
     pass
+
 
 def trainrnn():
     print(trainXY)
