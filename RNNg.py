dccda336368e462d86b6f792465dee661b6fb6f6
Vincc <changvincent57@gmail.com>
2021-05-16 23:42:40 +0800

    buildrnn

--- RNNg.py
+++ RNNg.py
@@ -24,7 +24,18 @@ trainSize = 10
 trainXY = [generateSet(i, 10, (1,10)) for i in range(1,trainSize)]
 
 def buildrnn():
-    pass
+    model = keras.Sequential()
+    # Add an Embedding layer expecting input vocab of size 1000, and
+    # output embedding dimension of size 64.
+    model.add(layers.Embedding(input_dim=1000, output_dim=64))
+
+    # Add a LSTM layer with 128 internal units.
+    model.add(layers.LSTM(128))
+
+    # Add a Dense layer with 10 units.
+    model.add(layers.Dense(10))
+
+    model.summary()
 
 
 def trainrnn():
